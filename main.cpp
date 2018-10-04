5aa7dadf7c72b69055e8375e021d5de3ae236af6
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2018-10-04 19:05:18 -0400

    Add files via upload
    Added test case for TTT_game class to main; a vector holding 100 TTT_game objects

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-
-using namespace std;
+#include "TTT_Game.h"
 
 int main()
 {
+    std::vector<TTT_Game> game_history;
+    //TTT_Game t;
+    for(int i = 0; i < 100; ++i)
+    {
+        game_history.push_back(TTT_Game());
+    }
 
+    for(TTT_Game t : game_history)
+    {
+        std::cout << t.checkWin() << "\n";
+
+    }
 }
